037ba12560c296cab39c38fd71e3699f9509d8fd
ZxYerSixZx <oroscoyeferson@gmail.com>
2024-07-04 21:01:37 -0500

    Actualizar youtube-ytmp3doc.js

--- plugins/youtube-ytmp3doc.js
+++ plugins/youtube-ytmp3doc.js
@@ -5,7 +5,7 @@ import fetch from 'node-fetch'
 let limit = 200
 
 let handler = async (m, { conn: star, args, text, isPrems, isOwner, usedPrefix, command }) => {
-if (!args || !args[0]) return star.reply(m.chat, '🚩 Ingresa el enlace del vídeo de YouTube junto al comando.\n\n`Ejemplo:`\n' + `> *${usedPrefix + command}* https://youtu.be/QSvaCSt8ixs`, m, rcanal)
+if (!args || !args[0]) return star.reply(m.chat, '🚩 Ingresa el enlace del vídeo de YouTube junto al comando.\n\n`Ejemplo:`\n' + `> *${usedPrefix + command}* https://youtu.be/q3jS4f9ItnE`, m, rcanal)
 if (!args[0].match(/youtu/gi)) return star.reply(m.chat, `Verifica que el enlace sea de YouTube.`, m, rcanal).then(_ => m.react('✖️'))
 let q = '128kbps'
 
@@ -21,9 +21,9 @@ let thumbnail = await yt.thumbnail
 let img = await (await fetch(`${thumbnail}`)).buffer()  
 if (size.split('MB')[0] >= limit) return star.reply(m.chat, `El archivo pesa mas de ${limit} MB, se canceló la Descarga.`, m, rcanal).then(_ => m.react('✖️'))
 	let txt = '`乂  Y O U T U B E  -  M P 3 D O C`\n\n'
-       txt += `	✩   *Titulo* : ${title}\n`
-       txt += `	✩   *Calidad* : ${q}\n`
-       txt += `	✩   *Tamaño* : ${size}\n\n`
+       txt += `	⚔️   *Titulo* : ${title}\n`
+       txt += `	⚔️  *Calidad* : ${q}\n`
+       txt += `	⚔️   *Tamaño* : ${size}\n\n`
        txt += `> *- ↻ El audio se esta enviando espera un momento, soy lenta. . .*`
 await star.sendFile(m.chat, img, 'thumbnail.jpg', txt, m, null, rcanal)
 await star.sendMessage(m.chat, { document: { url: dl_url }, caption: '', mimetype: 'audio/mpeg', fileName: `${title}.mp3`}, { quoted: m })
