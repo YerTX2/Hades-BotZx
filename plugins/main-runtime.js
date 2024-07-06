4ec27bd6385a584c4a63862983e7793a3db981a6
ZxYerSixZx <oroscoyeferson@gmail.com>
2024-07-06 02:46:24 -0500

    Actualizar main-runtime.js

--- plugins/main-runtime.js
+++ plugins/main-runtime.js
@@ -9,7 +9,7 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
       }) * 1000
     }
     let muptime = clockString(_muptime)
-   m.reply(`*» Bot activo durante* : ${muptime}`) 
+   m.reply(`*» Bot Hades activo durante* : ${muptime}`) 
 }
 handler.help = ['runtime']
 handler.tags = ['main']
