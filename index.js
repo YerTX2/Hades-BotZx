44959a4683314dc80303de08e1ea82816d6b8210
ZxYerSixZx <oroscoyeferson@gmail.com>
2024-07-07 18:25:18 -0500

    Actualizar index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const rl = createInterface(process.stdin, process.stdout)
 const app = express()
 const port = process.env.PORT || 8080;
 
-say('★ Hades-Bot ★\ZxYerSixZx', {
+say('Hades-Bot ', {
 font: 'chrome',
 align: 'center',
 gradient: ['red', 'magenta']})
