e549ebeac72c6681821cc24b93d4b1c410392c60
ZxYerSixZx <oroscoyeferson@gmail.com>
2024-07-10 16:10:37 -0500

    Update youtube-ytmp4.js

--- plugins/youtube-ytmp4.js
+++ plugins/youtube-ytmp4.js
@@ -7,7 +7,7 @@ let limit = 400
 let handler = async (m, { conn: star, args, text, isPrems, isOwner, usedPrefix, command }) => {
 if (!args || !args[0]) return star.reply(m.chat, '🚩 Ingresa el enlace del vídeo de YouTube junto al comando.\n\n`Ejemplo:`\n' + `> *${usedPrefix + command}* https://youtu.be/q3jS4f9ItnE`, m, rcanal)
 if (!args[0].match(/youtu/gi)) return star.reply(m.chat, `Verifica que el enlace sea de YouTube.`, m, rcanal).then(_ => m.react('✖️'))
-let q = args[1] || '720p'
+let q = args[1] || '1080p'
 
 await m.react('🕓')
 try {
